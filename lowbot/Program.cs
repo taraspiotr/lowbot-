d8195e97bb5a8bef1475d8093da135a2b968aa54
Piotr Tarasiewicz <taras.piotr@gmail.com>
2017-12-28 02:52:00 +0100

    rewrite class draw

--- lowbot/Program.cs
+++ lowbot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace lowbot
 {
@@ -6,7 +7,29 @@ namespace lowbot
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string Deck = Draw.GenerateDeck(5);
+            Console.WriteLine(Deck);
+
+            string Hand = Deck.Substring(0, 5);
+            Console.WriteLine(Hand);
+            Console.WriteLine(Draw.SortHand(Hand));
+
+            int[] numbers = new int[12] { 5, 5, 5, 7, 7, 7, 9, 7, 9, 9, 9, 1 };
+
+            var count = numbers
+                .GroupBy(e => e)
+                .OrderByDescending(e => e.Count())
+                .ThenByDescending(e => e.Key).ToList();
+            foreach (var el in count)
+                Console.WriteLine("{0} {1}", el.Key, el.Count());
+
+            Console.WriteLine(Draw.CompareHands("AKQ32", "T9854"));
+
+            string History = "rcf";
+            Console.WriteLine(Draw.GetCurrentPlayer(History));
+            Console.WriteLine(Draw.GetPotContribution(History, 1));
+
+        Console.ReadKey();
         }
     }
 }
